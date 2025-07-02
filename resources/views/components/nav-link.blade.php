f4d621945e02e63ae75ee2d801b9f117e1cd2454
Marek Toome <marekjah@gmail.com>
2025-07-02 07:37:25 +0300

    Day 5

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,10 +1,20 @@
-@props(['active' => false])
+@props(['active' => false, 'type' => 'a'])
 
 
-<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium "
+@if ($type === 'button')
+    <button
+        class="{{ $active ? 'bg-blue-900 text-green' : 'text-blue-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium "
+        {{ $attributes }}>{{ $slot }}</button>
+@else
+    <a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium "
+        aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}</a>
+@endif
+
+
+{{-- <a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium "
     aria-current="{{ $active ? 'page' : 'false' }}"
     {{ $attributes }}
-    >{{ $slot }}</a>
+    >{{ $slot }}</a> --}}
 
 {{-- <a
     {{ $attributes->merge([
