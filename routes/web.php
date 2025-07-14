dc1237934e45953f5684996ec4d67b93ec43e6c6
Marek Toome <marekjah@gmail.com>
2025-07-14 21:11:10 +0300

    Day 19

--- routes/web.php
+++ routes/web.php
@@ -1,98 +1,17 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
+// view
+// Route::get('/', function () {
+//     return view('index');
+// });
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-// index
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
-
-    return view('jobs.index', [
-        'jobs'=> $jobs
-]);
-});
+Route::resource('jobs', JobController::class);
 
 
-// create
-Route::get('/jobs/create', function (){
-    return view('jobs.create');
-});
-
-// show
-// {} see on wildcard mis kuulab kõike mida öeldakse peale jobs/,
-// wildcard sellepärast peaks olema lõpus
-Route::get('/jobs/{id}', function ($id) {
-        $job = Job::find($id);
-
-        return view('jobs.show',['job' => $job]);
-});
-
-// store
-Route::post('/jobs', function (){
-    request()->validate([
-        'title' =>['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-
-// edit
-Route::get('/jobs/{id}/edit', function ($id) {
-        $job = Job::find($id);
-
-        return view('jobs.edit',['job' => $job]);
-});
-
-// update
-Route::patch('/jobs/{id}', function ($id) {
-    // validate
-    request()->validate([
-        'title' =>['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    // authorize
-
-    // update the job
-    // and persist
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary')
-    ]);
-
-    // redirect to the job page
-    return redirect('/jobs/' . $job->id);
-
-});
-
-// delete
-Route::delete('/jobs/{id}', function ($id) {
-    // authorize
-
-    // delete the job
-    // $job = Job::faindOrFail($id);
-    // $job->delete();
-    Job::findOrFail($id)->delete();
-
-    // redirect
-    return redirect('/jobs');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
